36d053ee95e864785c69fb1c73eb8c12b3a37416
kundan karna <kundan.karna1994@gmail.com>
2021-08-15 20:31:01 +0545

    updated app

--- workbox-config.js
+++ workbox-config.js
@@ -1,10 +1,12 @@
 module.exports = {
+  cacheId: "HDA-v1.0.1",
   globDirectory: "build/",
   globPatterns: [
     "**/*.{css,scss,html,jpeg,txt,ttf,png,jpg,js,less,eot,svg,woff,woff2,json,md,gif,markdown,rb,css.map}",
   ],
-  navigateFallback:"/offline.html",
-  swDest: 'build/service-worker.js',
-//   swDest: 'build/sw.js',
-//   swSrc: "sw.js",
+  navigateFallback: "/offline.html",
+  swDest: "build/service-worker.js",
+  cleanupOutdatedCaches: true,
+  // mode: "production",
+  skipWaiting:true,
 };

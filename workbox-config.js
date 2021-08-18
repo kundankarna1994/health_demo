e1efa899be3d933270e720fa83d43800d13ee24a
kundan karna <kundan.karna1994@gmail.com>
2021-08-18 09:58:23 +0545

    updated app

--- workbox-config.js
+++ workbox-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  cacheId: "EHS-v1.0.8",
+  cacheId: "EHS-v1.0.9",
   globDirectory: "build/",
   globPatterns: [
     "**/*.{css,scss,html,jpeg,txt,ttf,png,jpg,js,less,eot,svg,woff,woff2,json,md,gif,markdown,rb,css.map}",
